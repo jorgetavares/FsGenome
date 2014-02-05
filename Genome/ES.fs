6fa75a896eeaa8b9543a942fae114569e983bc2f
Jorge Tavares <jorge.tavares@ieee.org>
2014-02-05 08:58:27 +0100

    Add skeleton for Evolutionary Strategies.
    Base structure for Self-Adaptive ES. Still incomplete.

---
+++ Genome/ES.fs
@@ -0,0 +1,17 @@
+﻿namespace Genome
+
+/// Genetic Algorithms
+module ES = 
+    open Core
+    open MathNet.Numerics.Random
+    open MathNet.Numerics.Distributions
+
+    type EvolutionaryStrategy() =      
+        /// ES  
+        static member RunSelfAdaptive (parameters: Parameters, fitnessFunction, ?random: System.Random) =
+            let rng = defaultArg random (Random.mersenneTwisterSeed parameters.Seed) 
+            let chromossomeSize = parameters.ChromossomeSize * 2    // plus self-adaptive mutation rate for each gene
+            let chromossomeBuilder = (fun () -> LinearChromossome.randomFloat chromossomeSize rng)
+            rng
+            // TODO: change baseEA to allow mutation only and use the self=adaptive params
+        
