4f59f681ae7b8ccacdd06db797d4b518a943e929
Jorge Tavares <jorge.tavares@ieee.org>
2014-02-15 19:56:28 +0100

    Add simple grammar builder for GE.

--- Genome/GE.fs
+++ Genome/GE.fs
@@ -3,7 +3,19 @@
 /// Grammatical Evolution
 module GE = 
     open Core
+    open System.Collections.Generic
+    open System.Text.RegularExpressions
     open MathNet.Numerics.Random
+
+    /// returns a dict that represents a grammar
+    type Grammar(grammar: string[]) = 
+        let parseGrammar grammar = 
+            let rules = new Dictionary<string, string array>()
+            grammar
+            |> Array.map (fun r -> Regex.Split(r, ":=")) 
+            |> Array.iter (fun r -> rules.Add(r.[0], r.[1].Split('|')))  
+
+        member val Rules = parseGrammar grammar with get
 
     /// TODO: complete basic implementation    
     type GrammaticalEvolution() =      
